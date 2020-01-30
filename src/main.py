86f667be86ea4e946eedacba43d4145189b73d59
SiawPeng Er <ser8@gatech.edu>
2020-01-29 21:39:46 -0500

    bug fixed

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 import random
 import params
 import World
+import Room
 
 # Seed the random number generator
 random.seed(1)
@@ -14,6 +15,7 @@ logfile = open(parameters['logfilename'], 'w')
 
 # Create the world
 world = World.World(parameters, logfile)
+Room.InitRoom(world)
 
 # Run for twelve years
 while world.has_events() and world.day < 365 * 12:
